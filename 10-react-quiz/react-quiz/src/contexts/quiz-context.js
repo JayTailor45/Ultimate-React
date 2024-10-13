2e91ed6e7fb6dce79abb1e2c4399364b2d90ad24
khushboo1608 <khushboo.vbinfotech@gmail.com>
2024-10-13 05:20:14 -0700

    wip context

---
+++ 10-react-quiz/react-quiz/src/contexts/quiz-context.js
@@ -0,0 +1,15 @@
+import { useContext } from "react"
+
+const QuizContext = useContext();
+
+const initialState = {
+
+};
+
+function reducer(state, action) {
+
+}
+
+function useQuiz() {
+  const context = useContext()
+}
