b898e078fab22e59efd927e09afa47181ac64ae0
khushboo1608 <khushboo.vbinfotech@gmail.com>
2024-11-17 03:04:22 -0800

    add core styled components

--- 17-the-wild-oasis/wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/wild-oasis/src/App.jsx
@@ -1,13 +1,45 @@
-import { useState } from "react";
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>Hello World!</div>
+      <GlobalStyles />
+      <StyledApp>
+        <Row type="horizontal"></Row>
+        <Row type="vertical"></Row>
+        <Heading as="h1">Hello World!</Heading>
+        <Button
+          variation="primary"
+          size="medium"
+          onClick={() => alert("check ")}
+        >
+          Check In
+        </Button>
+        <Button variation="danger" size="small" onClick={() => alert("check ")}>
+          Check In
+        </Button>
+        <Heading as="h2">Hello World!</Heading>
+        <Heading as="h3">Hello World!</Heading>
+        <Input type="number" placeholder="Number of guests"></Input>
+      </StyledApp>
     </>
   );
 }
+
+Button.defaultProps = {
+  variation: "primary",
+  size: "medium",
+};
 
 export default App;
