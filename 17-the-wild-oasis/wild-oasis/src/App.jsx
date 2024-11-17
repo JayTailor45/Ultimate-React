26074df7f1edd003f46bee23dadd78e455c8c1b4
khushboo1608 <khushboo.vbinfotech@gmail.com>
2024-11-17 03:17:51 -0800

    setup routes

--- 17-the-wild-oasis/wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/wild-oasis/src/App.jsx
@@ -1,45 +1,36 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+
 import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
-
-import Button from "./ui/Button";
-import Input from "./ui/Input";
-import Heading from "./ui/Heading";
-import Row from "./ui/Row";
-
-const StyledApp = styled.div`
-  background-color: orangered;
-  padding: 20px;
-`;
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <StyledApp>
-        <Row type="horizontal"></Row>
-        <Row type="vertical"></Row>
-        <Heading as="h1">Hello World!</Heading>
-        <Button
-          variation="primary"
-          size="medium"
-          onClick={() => alert("check ")}
-        >
-          Check In
-        </Button>
-        <Button variation="danger" size="small" onClick={() => alert("check ")}>
-          Check In
-        </Button>
-        <Heading as="h2">Hello World!</Heading>
-        <Heading as="h3">Hello World!</Heading>
-        <Input type="number" placeholder="Number of guests"></Input>
-      </StyledApp>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate relative to="dashboard" />}></Route>
+          <Route path="dashboard" element={<Dashboard />}></Route>
+          <Route path="bookings" element={<Bookings />}></Route>
+          <Route path="cabins" element={<Cabins />}></Route>
+          <Route path="users" element={<Users />}></Route>
+          <Route path="settings" element={<Settings />}></Route>
+          <Route path="account" element={<Account />}></Route>
+          <Route path="login" element={<Login />}></Route>
+          <Route path="*" element={<PageNotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
-
-Button.defaultProps = {
-  variation: "primary",
-  size: "medium",
-};
 
 export default App;
