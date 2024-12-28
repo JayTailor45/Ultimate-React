9b5c468237778dd86e0ea418b9784e617828ab77
JAY TAILOR <tailorj64@gmail.com>
2024-12-28 21:11:38 +0530

    init supabase connection

---
+++ 17-the-wild-oasis/wild-oasis/src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://lvnoqoejuvxlhovdzzpp.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx2bm9xb2VqdXZ4bGhvdmR6enBwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUzODIzODUsImV4cCI6MjA1MDk1ODM4NX0.mffhKZgM9Szzdynn7dtGwh2ZFMbCw3dMzUcCMcng-eI';
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
