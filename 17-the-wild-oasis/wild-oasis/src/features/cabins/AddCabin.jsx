5de821f85713da90976abcb0fca3e245019c6746
JAY TAILOR <tailorj64@gmail.com>
2025-01-11 19:27:29 +0530

    Modal using compound component approach and context for data-function sharing

--- 17-the-wild-oasis/wild-oasis/src/features/cabins/AddCabin.jsx
+++ 17-the-wild-oasis/wild-oasis/src/features/cabins/AddCabin.jsx
@@ -4,24 +4,37 @@ import Button from "../../ui/Button";
 import Modal from "../../ui/Modal";
 
 function AddCabin() {
-  const [showModal, setShowModal] = useState(false);
-
   return (
-    <>
-      <Button
-        variation="primary"
-        size="medium"
-        onClick={() => setShowModal(!showModal)}
-      >
-        Add new cabins
-      </Button>
-      {showModal && (
-        <Modal onClose={() => setShowModal(false)}>
-          <CreateCabinForm onClose={() => setShowModal(false)} />
-        </Modal>
-      )}
-    </>
+    <Modal>
+      <Modal.Open opens="cabin-form">
+        <Button variation="primary" size="medium">Add new cabin</Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateCabinForm />
+      </Modal.Window>
+    </Modal>
   );
 }
+
+// function AddCabin() {
+//   const [showModal, setShowModal] = useState(false);
+
+//   return (
+//     <>
+//       <Button
+//         variation="primary"
+//         size="medium"
+//         onClick={() => setShowModal(!showModal)}
+//       >
+//         Add new cabins
+//       </Button>
+//       {showModal && (
+//         <Modal onClose={() => setShowModal(false)}>
+//           <CreateCabinForm onClose={() => setShowModal(false)} />
+//         </Modal>
+//       )}
+//     </>
+//   );
+// }
 
 export default AddCabin;
--- 17-the-wild-oasis/wild-oasis/src/ui/ModalV1.jsx
+++ 17-the-wild-oasis/wild-oasis/src/ui/ModalV1.jsx
@@ -1,8 +1,6 @@
-import { cloneElement, createContext, useContext, useState } from "react";
+import { HiXMark } from "react-icons/hi2";
 import { createPortal } from "react-dom";
 import styled from "styled-components";
-
-import { HiXMark } from "react-icons/hi2";
 
 const StyledModal = styled.div`
   position: fixed;
@@ -53,42 +51,18 @@ const Button = styled.button`
   }
 `;
 
-const ModalContext = createContext();
-
-function Modal({ children }) {
-  const [openName, setOpenName] = useState("");
-
-  const close = () => setOpenName("");
-  const open = setOpenName;
-  return (
-    <ModalContext.Provider value={{ openName, close, open }}>
-      {children}
-    </ModalContext.Provider>
-  );
-}
-
-function Open({ children, opens }) {
-  const { open } = useContext(ModalContext);
-  return cloneElement(children, { onClick: () => open(opens) });
-}
-
-function Window({ name, children }) {
-  const { openName, close } = useContext(ModalContext);
-  if (name !== openName) return null;
+function Modal({ onClose, children }) {
   return createPortal(
     <Overlay>
       <StyledModal>
-        <Button onClick={close}>
+        <Button onClick={onClose}>
           <HiXMark />
         </Button>
-        <div>{cloneElement(children, { onClose: close })}</div>
+        <div>{children}</div>
       </StyledModal>
     </Overlay>,
     document.body
   );
 }
-
-Modal.Open = Open;
-Modal.Window = Window;
 
 export default Modal;
