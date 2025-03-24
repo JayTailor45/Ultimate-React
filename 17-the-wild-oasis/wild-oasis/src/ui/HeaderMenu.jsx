2f79323a3df55514402ed5d49aed185376f1364c
JayTailor45 <tailorj64@gmail.com>
2025-03-24 10:09:28 +0530

    add header

---
+++ 17-the-wild-oasis/wild-oasis/src/ui/HeaderMenu.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Logout from "../features/authentication/Logout";
+import ButtonIcon from "./ButtonIcon";
+import { HiOutlineUser } from "react-icons/hi2";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const StyledHeaderMenu = styled.ul`
+  display: flex;
+  gap: 0.4 rem;
+`;
+
+export default function HeaderMenu() {
+  const navigate = useNavigate();
+  return (
+    <StyledHeaderMenu>
+      <li>
+        <ButtonIcon onClick={() => navigate("/account")}>
+          <HiOutlineUser />
+        </ButtonIcon>
+      </li>
+      <li>
+        <Logout />
+      </li>
+    </StyledHeaderMenu>
+  );
+}
