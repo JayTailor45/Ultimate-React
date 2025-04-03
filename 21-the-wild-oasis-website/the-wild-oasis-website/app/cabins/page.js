b3b75fa8e2ed45116796dc39fbe38e0f20a62788
JayTailor45 <tailorj64@gmail.com>
2025-04-03 10:32:14 +0530

    basic data fetching and server and client component

--- 21-the-wild-oasis-website/the-wild-oasis-website/app/cabins/page.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/cabins/page.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
+import Counter from "../components/Counter";
 
-export default function Page() {
+export default async function Page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  console.log(`Server`, data);
   return (
-    <h1>Cabins</h1>
-  )
+    <>
+      <h1>Cabins</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+      <Counter users={data} />
+    </>
+  );
 }
