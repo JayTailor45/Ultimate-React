8319f17c3812dde229b0e4922e37a170c6381528
JayTailor45 <tailorj64@gmail.com>
2025-04-06 15:12:58 +0530

    add cache revalidation for cabins

--- 21-the-wild-oasis-website/the-wild-oasis-website/app/cabins/page.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/cabins/page.js
@@ -2,6 +2,8 @@ import React, { Suspense } from "react";
 import CabinList from "@/app/_components/CabinList";
 import Spinner from "@/app/_components/Spinner";
 
+export const revalidate = 3600;
+
 export const metadata = {
   title: "Cabins",
   description: "Book your dream cabin",
