5b523bf79df060b124f0d730b21cced86e917db4
JayTailor45 <tailorj64@gmail.com>
2025-04-13 11:06:53 +0530

    add cabin filters

--- 21-the-wild-oasis-website/the-wild-oasis-website/app/_components/CabinList.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/_components/CabinList.js
@@ -2,12 +2,24 @@ import React from "react";
 import CabinCard from "@/app/_components/CabinCard";
 import { getCabins } from "@/app/_libs/data-service";
 
-export default async function CabinList() {
+export default async function CabinList({ filter }) {
   const cabins = await getCabins();
   if (!cabins?.length) return null;
+  let displayedCabins = [];
+  if (filter === "all") {
+    displayedCabins = cabins;
+  } else if (filter === "small") {
+    displayedCabins = cabins.filter((cabin) => cabin.maxCapacity <= 3);
+  } else if (filter === "medium") {
+    displayedCabins = cabins.filter(
+      (cabin) => cabin.maxCapacity >= 4 && cabin.maxCapacity <= 7
+    );
+  } else if (filter === "large") {
+    displayedCabins = cabins.filter((cabin) => cabin.maxCapacity >= 8);
+  }
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-      {cabins.map((cabin) => (
+      {displayedCabins.map((cabin) => (
         <CabinCard cabin={cabin} key={cabin.id} />
       ))}
     </div>
--- 21-the-wild-oasis-website/the-wild-oasis-website/app/cabins/page.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/cabins/page.js
@@ -1,6 +1,7 @@
 import React, { Suspense } from "react";
 import CabinList from "@/app/_components/CabinList";
 import Spinner from "@/app/_components/Spinner";
+import Filter from "../_components/filter";
 
 export const revalidate = 3600;
 
@@ -9,7 +10,8 @@ export const metadata = {
   description: "Book your dream cabin",
 };
 
-export default function Page() {
+export default function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? "all";
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -19,13 +21,15 @@ export default function Page() {
         Cozy yet luxurious cabins, located right in the heart of the Italian
         Dolomites. Imagine waking up to beautiful mountain views, spending your
         days exploring the dark forests around, or just relaxing in your private
-        hot tub under the stars. Enjoy nature&apos;s beauty in your own little home
-        away from home. The perfect spot for a peaceful, calm vacation. Welcome
-        to paradise.
+        hot tub under the stars. Enjoy nature&apos;s beauty in your own little
+        home away from home. The perfect spot for a peaceful, calm vacation.
+        Welcome to paradise.
       </p>
-
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
