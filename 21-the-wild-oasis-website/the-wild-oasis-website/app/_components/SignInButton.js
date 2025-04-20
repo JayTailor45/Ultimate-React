4de2caf24cad7507d32bcdaf071c906c478fda70
JayTailor45 <tailorj64@gmail.com>
2025-04-20 17:55:37 +0530

    logout and login flow implemented

--- 21-the-wild-oasis-website/the-wild-oasis-website/app/_components/SignInButton.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/_components/SignInButton.js
@@ -1,20 +1,23 @@
 import Image from "next/image";
+import { signInAction } from "../_libs/actions";
 
 function SignInButton() {
   return (
-    <button className="flex items-center gap-6 text-lg border border-primary-300 px-10 py-4 font-medium">
-      <div className="flex-1 relative">
-        <Image
-          fill
-          className="object-cover"
-          src="https://authjs.dev/img/providers/google.svg"
-          alt="Google logo"
-          height="24"
-          width="24"
-        />
-      </div>
-      <span>Continue with Google</span>
-    </button>
+    <form action={signInAction}>
+      <button className="flex items-center gap-6 text-lg border border-primary-300 px-10 py-4 font-medium">
+        <div className="flex-1 relative">
+          <Image
+            // fill
+            className="object-cover"
+            src="https://authjs.dev/img/providers/google.svg"
+            alt="Google logo"
+            height="24"
+            width="24"
+          />
+        </div>
+        <span>Continue with Google</span>
+      </button>
+    </form>
   );
 }
 
--- 21-the-wild-oasis-website/the-wild-oasis-website/app/account/page.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/account/page.js
@@ -1,15 +1,17 @@
 import React from "react";
+import { auth } from "../_libs/auth";
 
 export const metadata = {
   title: "Account",
   description: "Update your account info",
 };
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
   return (
     <div>
       <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-        Your reservations
+        Welcome, {session?.user?.name}
       </h2>
     </div>
   );
